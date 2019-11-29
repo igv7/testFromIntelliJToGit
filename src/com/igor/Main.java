ffc329fa3d9fa50d3cffe57f8f743468974d8690
igv7 <46227257+igv7@users.noreply.github.com>
2019-11-30 01:41:39 +0200

    Second Commit Adding sout string "Hello World!"

--- src/com/igor/Main.java
+++ src/com/igor/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //Second Commit --> Adding sout string "Hello World!"
+        System.out.println("Hello World!");
     }
 }
